071ed0e69d4617364753621d9e31db962d1f48f4
AkRs660 <andrekraemer94@gmail.com>
2026-08-11 16:35:11 +0200

    Reset scroll on route changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop";
 import { App } from "./pages/App";
 import { ProjectPage } from "./pages/ProjectPage";
 import "./styles.css";
@@ -8,6 +9,7 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/projects/:slug" element={<ProjectPage />} />
